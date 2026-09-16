88329f9847d824cce7f04643798c9aef80d2e802
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-16 16:11:21 -0300

    fix: import Compose saveable state API

--- app/src/main/java/cl/iac33/app/MainActivity.kt
+++ app/src/main/java/cl/iac33/app/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
