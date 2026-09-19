6e506582037f220a5f9f58adf05368a024c2e89a
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-19 02:29:28 -0300

    fix: import Compose vertical scroll

--- app/src/main/java/cl/iac33/app/MultimediaEditor.kt
+++ app/src/main/java/cl/iac33/app/MultimediaEditor.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
